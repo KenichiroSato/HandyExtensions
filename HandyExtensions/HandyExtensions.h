64142fca210c9c8e97292945947682fb69467db3
Kenichiro Sato <satok0702@gmail.com>
2016-07-31 11:44:52 +0900

    Initial Commit

---
+++ HandyExtensions/HandyExtensions.h
@@ -0,0 +1,19 @@
+//
+//  HandyExtensions.h
+//  HandyExtensions
+//
+//  Created by 佐藤健一朗 on 2016/07/31.
+//  Copyright © 2016年 Kenichiro Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HandyExtensions.
+FOUNDATION_EXPORT double HandyExtensionsVersionNumber;
+
+//! Project version string for HandyExtensions.
+FOUNDATION_EXPORT const unsigned char HandyExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandyExtensions/PublicHeader.h>
+
+
